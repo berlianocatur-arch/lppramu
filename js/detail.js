a4be2e71e575396edba1e11b974b4ef29d137bb5
Berliano <Berliano.535190078@stu.untar.ac.id>
2026-03-24 16:53:38 +0700

    Update Landing Page

--- js/detail.js
+++ js/detail.js
@@ -1,26 +1,98 @@
-const id = new URLSearchParams(location.search).get("id")
+const params = new URLSearchParams(window.location.search)
+const name = params.get("name")
+
+let selectedCar = null
 
 fetch("data/cars.json")
 .then(res => res.json())
 .then(cars => {
 
-const car = cars.find(c => c.id == id)
+const car = cars.find(c => c.name === name)
+selectedCar = car
 
-document.getElementById("detail").innerHTML = `
-<h1>${car.name}</h1>
-<img src="${car.image}" width="300">
-<p>${car.description}</p>
-`
+if(!car){
+document.body.innerHTML = "Mobil tidak ditemukan"
+return
+}
 
-window.selectedCar = car.name
+/* isi data */
+/* isi data */
+document.getElementById("name").innerText = car.name
+
+let priceEl = document.getElementById("price")
+let oldPriceEl = document.getElementById("oldPrice")
+
+if(car.variants){
+
+  let html = ""
+
+  let cheapest = Math.min(...car.variants.map(v => v.price))
+
+  car.variants.forEach(v => {
+    html += `
+      <div class="variant">
+        <span class="variant-name">${v.name}</span>
+        <span class="variant-price ${v.price === cheapest ? 'best' : ''}">
+          Rp ${v.price.toLocaleString()}
+        </span>
+        ${v.price === cheapest ? '<span class="badge-best">BEST DEAL</span>' : ''}
+      </div>
+      `
+  })
+
+  priceEl.innerHTML = html
+  oldPriceEl.style.display = "none"
+
+} else {
+
+  priceEl.innerText = "Rp " + car.price.toLocaleString()
+  oldPriceEl.innerText = "Rp " + car.oldPrice.toLocaleString()
+
+}
+
+document.getElementById("desc").innerText = car.description
+/* gambar utama */
+if(car.images && car.images.length > 0){
+  document.getElementById("mainImage").src = car.images[0]
+}
+
+/* gallery */
+let gal = ""
+
+if(car.images){
+  car.images.forEach(img=>{
+    gal += `<img src="${img}" onclick="changeImage('${img}')">`
+  })
+}
+
+document.getElementById("gallery").innerHTML = gal
+
+/* specs */
+let specHtml = ""
+
+if(car.specs){
+  for(let key in car.specs){
+    specHtml += `
+    <tr>
+      <td>${key}</td>
+      <td>${car.specs[key]}</td>
+    </tr>
+    `
+  }
+}
+
+document.getElementById("specs").innerHTML = specHtml
 
 })
 
+function changeImage(src){
+document.getElementById("mainImage").src = src
+}
+
+/* WA */
 function wa(){
 
-let nama = document.getElementById("nama").value
-
-let text = `Halo saya ${nama} tertarik dengan ${window.selectedCar}`
+let text = `Halo saya tertarik dengan ${selectedCar.name}`
 
 window.open(
 `https://wa.me/628123456789?text=${encodeURIComponent(text)}`
--- js/main.js
+++ js/main.js
@@ -9,11 +9,15 @@ cars.forEach(car => {
 
 html += `
 <div class="card">
-<img src="${car.image}">
+
+<img src="${car.images && car.images.length ? car.images[0] : 'images/default.jpg'}">
+
 <h3>${car.name}</h3>
+
 <p>Rp ${car.price.toLocaleString()}</p>
 
-<a href="detail.html?id=${car.id}">Detail</a>
+<a href="detail.html?name=${encodeURIComponent(car.name)}">Detail</a>
+
 </div>
 `
 
